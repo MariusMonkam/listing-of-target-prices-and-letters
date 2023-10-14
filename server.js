7c9fe5a57a122fa60f5661d6d05906126f04fc93
MariusMonkam <cloudmonde@yahoo.com>
2023-10-14 18:50:24 +0300

    Update deployment script to build frontend before serving

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require("cors"); // Import the cors middleware
+const cors = require("cors");
+const path = require("path"); // Import the path module
 
 // Import your routes
 const targetPriceRoutes = require("./routes/targetPricesRoutes");
@@ -11,12 +12,15 @@ app.use(cors());
 // Use the routes
 app.use("/api", targetPriceRoutes);
 
-const port = process.env.PORT || 5000; // You can change the port as needed
+// Serve the static frontend files
+app.use(express.static(path.join(__dirname, "client/build")));
 
-// Define a route
-app.get("/", (req, res) => {
-  res.send("Hello, this is your Express server!");
+// Define a route to handle frontend requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
+
+const port = process.env.PORT || 5000;
 
 // Start the server
 app.listen(port, () => {
